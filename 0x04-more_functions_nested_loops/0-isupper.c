27eb4417f76f856a9e985487300d26a124babbfb
engulater <engulater@gmail.com>
2023-02-23 16:35:33 -0800

    task 10

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 
 /**
  * _isupper - uppercase letters
@@ -10,6 +10,8 @@ i#include "main.h"
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0)
+	{
+	return (1);
+	}
+	return (0);
+}
